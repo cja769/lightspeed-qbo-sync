6cca553e4c611731cb8ee8b8adb56aed03d9b1a5
Jay Aisenbrey <caisenbrey@obm.mn>
2026-09-18 13:14:50 -0500

    Create LightspeedSyncApplication.java

---
+++ src/main/java/io/github/thirdcoast/lightspeed/LightspeedSyncApplication.java
@@ -0,0 +1,9 @@
+package io.github.thirdcoast.lightspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightspeedSyncApplication {
+    public static void main(String[] args) { SpringApplication.run(LightspeedSyncApplication.class, args); }
+}
